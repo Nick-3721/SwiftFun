fde972c223fc4ea42f24659fed97c921c342fb4e
Nicholas Macdonald <Nicholasmacdonalddesign@gmail.com>
2020-01-02 17:36:58 +0000

    Initial Commit

---
+++ 2020 Swift fun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2020 Swift fun
+//
+//  Created by Nicholas MacDonald on 1/2/20.
+//  Copyright © 2020 Nicholas MacDonald. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
